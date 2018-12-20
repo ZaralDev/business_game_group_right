438dde1be695210946fe4df1cd8a2447c996ef9d
Zaral <samuelmarchese7@gmail.com>
2018-12-20 14:13:20 +0100

    Update ports

--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home pages. */
 router.get('/', function (req, res, next) {
-  res.render('registter', {});
+  res.render('register', {});
 });
 
 module.exports = router;
